c0a682b5ea8191667783ded3df482b472f7b47bc
Your Name <you@example.com>
2023-03-02 14:08:44 -0800

    updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ * leet - encodes a string to 1337
+ * @str: the string to be encoded
+ * Return: a pointer to the encoded string
  */
-char *leet(char *n)
+char *leet(char *str)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (str[i] == s1[j])
 			{
-				n[i] = s2[i];
+				str[i] = s2[i];
 			}
 		}
 	}
-	return (n);
+	return (str);
 }
